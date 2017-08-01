ff5a3022e7a976d4058e3dfef5166773e659413c
Michael Roque <mycroquet@gmail.com>
2017-07-31 20:47:52 -0600

    final weight for weight

--- 5kyu/weightforweight.js
+++ 5kyu/weightforweight.js
@@ -2,18 +2,26 @@ function orderWeight(strng) {
 
     var tokenArray = strng.split(' ');
     const mapper = str => {
-      return str.split('').map(Number).reduce(add, 0);
+      return str.split('').map(Number).reduce(add);
     };
+
     var int = tokenArray.sort((a,b) => {
-      return mapper(a) - mapper(b);
+      let mappedA =  mapper(a);
+      let mappedB = mapper(b);
+      if(mappedA === mappedB){
+        return a.localeCompare(b);
+
+      }
+      return mappedA - mappedB;
     });
-    var result = int.join(" ")
+    var result = int.join(' ');
 
       console.log(result)
 
          function add(a,b){
           return a + b
         }
+
 
       return result
 
