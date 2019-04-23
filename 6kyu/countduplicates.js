259c0d6c518f81a1ad5eb02dd72e5a10dfad02a0
Michael Roque <mycroquet@gmail.com>
2019-04-22 22:24:26 -0600

    updated answer

--- 6kyu/countduplicates.js
+++ 6kyu/countduplicates.js
@@ -1,24 +1,4 @@
 function duplicateCount(text){
-  //...
-  let arr = text.toLowerCase().split("").sort()
-  let result = []
-
-  if(arr.length < 1) {
-    return 0
-  }
-  else {
-    arr.forEach(function(element, index) {
-
-    // Find if there is a duplicate or not
-    if (arr.indexOf(element, index + 1) > -1) {
-
-      // Find if the element is already in the result array or not
-      if (result.indexOf(element) === -1) {
-        result.push(element);
-      }
-    }
-  })
-    if(result.length < 1) return 0
-  }
-  return result.length
-}
+  return text.toLowerCase().split("").filter(function(val, i, arr){
+    return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
+  }).length;
